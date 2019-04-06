9a4b759c57938a437207cd18ea55e613a0c0f487
andrew <Andrey>
2019-04-06 20:49:49 +0300

    Notepad v.1, working

--- memo.rb
+++ memo.rb
@@ -2,10 +2,23 @@
 class Memo < Post
 
   def read_from_console
+    puts "Новая заметка (все, что пишите до строчки \"end\"):"
+
+    @text = []
+    line = nil
+
+    while line != "end" do
+      line = STDIN.gets.chomp
+      @text << line
+    end
+
+    @text.pop  # удаляем последнюю строку - end
 
   end
 
   def to_strings
-    
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+    return @text.unshift(time_string)  # добавляет в начало массива строку time_string
   end
 end
